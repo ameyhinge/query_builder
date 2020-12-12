63b264f119cf6cbdb9e3ce78e89c1dc727961ab7
ameyhinge <ameyhinge@gmail.com>
2020-12-12 10:47:03 +0530

    Added test data

--- abcd.go
+++ abcd.go
@@ -2,8 +2,20 @@ package querybuilder
 
 import (
 	"fmt"
+	"reflect"
 )
 
 func Hello() {
 	fmt.Println("Hello, World!")
+}
+
+type Employee struct {
+	id   int
+	name string
+}
+
+func parseStruct(emp Employee) {
+	for i := 0; i < reflect.ValueOf(emp).NumField(); i++ {
+		fmt.Println(reflect.ValueOf(emp).Field(i))
+	}
 }
